51ca482ed1e911e08600332d79012df3e4a31eaa
Juan Diego Bernal Pedroza <bernaljuandiego@hotmail.com>
2019-11-09 10:44:37 -0500

    salto con poder

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -34,7 +34,15 @@ public class PlayerMovement : MonoBehaviour {
 			crouch = false;
 		}
 
-	}
+        if (Input.GetButtonDown("Fire1"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 30;
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, -3);
+            StartCoroutine(CuentaRegresivaPoder(1));
+        }
+
+    }
 
 	public void OnLanding ()
 	{
@@ -51,5 +59,20 @@ public class PlayerMovement : MonoBehaviour {
 		// Move our character
 		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
 		jump = false;
-	}
+
+    }
+
+    float tiempoPoder;
+    public IEnumerator CuentaRegresivaPoder(float tiempoCuentaAtras)
+    {
+        tiempoPoder = tiempoCuentaAtras;
+        while (tiempoPoder > 0)
+        {
+            Debug.Log("CuentaAtras: " + tiempoPoder);
+            yield return new WaitForSeconds(1.0f);
+            tiempoPoder--;
+            GetComponent<Rigidbody2D>().gravityScale = 3;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, +3);
+        }
+    }
 }
